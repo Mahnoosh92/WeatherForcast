574e4f07a2412ea74f00a365ed02fbc408bf5c60
mahnoosh92 <mahnooshmahdavi2012@gmail.com>
2025-12-03 07:44:35 +0000

    code clean-up

--- core/data/src/main/java/com/mahnoosh/data/repository/city/CityRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.mahnoosh.data.repository.city
-
-import com.mahnoosh.model.data.City
-
-interface CityRepository {
-    suspend fun fetchCities(
-        q: String,
-    ): Result<List<City>>
-}
--- core/data/src/main/java/com/mahnoosh/data/repository/weather/WeatherRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.mahnoosh.data.repository.weather
-
-import com.mahnoosh.model.data.CurrentWeather
-
-interface WeatherRepository {
-    suspend fun fetchCurrentWeather(
-        q: String
-    ): Result<CurrentWeather>
-}
